caca5c5f2e685cee9f4a3c1943ee32912826edbe
LRN251 <150847992+LRN251@users.noreply.github.com>
2023-12-27 17:41:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#Excuse").innerHTML = Message();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let Message = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let i_who = Math.floor(Math.random() * who.length);
+  let i_action = Math.floor(Math.random() * action.length);
+  let i_what = Math.floor(Math.random() * what.length);
+  let i_when = Math.floor(Math.random() * when.length);
+  return (
+    who[i_who] +
+    " " +
+    action[i_action] +
+    " " +
+    what[i_what] +
+    " " +
+    when[i_when]
+  );
 };
